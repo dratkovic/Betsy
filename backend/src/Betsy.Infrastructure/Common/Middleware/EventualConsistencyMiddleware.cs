aaa3b1fc096094e8aa709a32f6ef6d3a3da977c9
Damir Ratkovic <ratkovic.damir@gmail.com>
2024-09-13 22:07:33 +0200

    Fixed eventual consistency opening transaction part

--- backend/src/Betsy.Infrastructure/Common/Middleware/EventualConsistencyMiddleware.cs
+++ backend/src/Betsy.Infrastructure/Common/Middleware/EventualConsistencyMiddleware.cs
@@ -10,36 +10,36 @@ public class EventualConsistencyMiddleware(RequestDelegate next)
 {
     public async Task InvokeAsync(HttpContext context, IPublisher publisher, BetsyDbContext dbContext)
     {
-        // This is a queue of domain events that will be published after the response is sent to the client
-        // Usually this is done in DbContext on save. In that case user is waiting for the response until
-        // all the domain events are published and handled. I like this approach better because it doesn't
-        // block the user from getting the response. Consequence if failure occur is that user will not get
-        // that error response immediately but it will be handled in the background.
-        context.Response.OnCompleted(async () =>
+        var strategy = dbContext.Database.CreateExecutionStrategy();
+
+        await strategy.ExecuteAsync(async () =>
+        {
+            using var transaction = dbContext.Database.BeginTransaction();
+            // This is a queue of domain events that will be published after the response is sent to the client
+            // Usually this is done in DbContext on save. In that case user is waiting for the response until
+            // all the domain events are published and handled. I like this approach better because it doesn't
+            // block the user from getting the response. Consequence if failure occur is that user will not get
+            // that error response immediately but it will be handled in the background.
+            context.Response.OnCompleted(async () =>
         {
             try
             {
-                var strategy = dbContext.Database.CreateExecutionStrategy();
-
-                await strategy.ExecuteAsync(async () =>
+                if (context.Items.TryGetValue("DomainEventsQueue", out var value) &&
+                    value is Queue<IDomainEvent> domainEventsQueue)
+                {
+                    while (domainEventsQueue!.TryDequeue(out var domainEvent))
                     {
-                        using var transaction = dbContext.Database.BeginTransaction();
-                        if (context.Items.TryGetValue("DomainEventsQueue", out var value) &&
-                            value is Queue<IDomainEvent> domainEventsQueue)
-                        {
-                            while (domainEventsQueue!.TryDequeue(out var domainEvent))
-                            {
-                                await publisher.Publish(domainEvent);
-                            }
-                        }
+                        await publisher.Publish(domainEvent);
+                    }
+                }
 
-                        await transaction.CommitAsync();
-                    });
+                await transaction.CommitAsync();
             }
             catch (Exception)
             {
                 // TODO Implement resiliency.. Send an email / Add to some queue for reprocessing...
             }
+        });
         });
 
         await next(context);
